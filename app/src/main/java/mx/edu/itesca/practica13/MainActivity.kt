99aaca56b59ee3099311950ea691804a93a3eb26
Multimedia <Multimedia@cajeme.edu.mx>
2024-04-15 15:37:08 -0700

    Initial commit

---
+++ app/src/main/java/mx/edu/itesca/practica13/MainActivity.kt
@@ -0,0 +1,57 @@
+package mx.edu.itesca.practica13
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import com.google.firebase.database.ktx.database
+
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private val File = 1
+    private val database = Firebase.database
+    val myRef = database.getReference("user")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val uploadImageView: ImageView=findViewById(R.id.uploadImageView)
+        uploadImageView.setOnClickListener {
+            fileUpload()
+        }
+    }
+
+    fun fileUpload() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        startActivityForResult(intent, File)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == File) {
+            if (resultCode == RESULT_OK) {
+                val FileUri = data!!.data
+                val Folder: StorageReference =
+                    FirebaseStorage.getInstance().getReference().child("User")
+                val file_name: StorageReference = Folder.child("file" + FileUri!!.lastPathSegment)
+                file_name.putFile(FileUri).addOnSuccessListener { taskSnapshot ->
+                    file_name.getDownloadUrl().addOnSuccessListener { uri ->
+                        val hashMap =
+                            HashMap<String, String>()
+                        hashMap["link"] = java.lang.String.valueOf(uri)
+                        myRef.setValue(hashMap)
+                        Log.d("Mensaje", "Se subió correctamente")
+                    }
+                }
+            }
+        }
+    }
+
+
+}
